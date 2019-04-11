4bb0e1001a830b413e7eea76cf6bec6d4fcacaa1
abdullahalnoor007 <aalnoor74@gmail.com>
2019-04-11 23:12:32 +0600

    update

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -17,6 +17,15 @@ class TextController extends Controller
     public function store(Request $request){
         $text = new Text();
 
+        if($request->hasFile('fav_icon')){
+            @unlink(asset('frontend/images/icon.png'));
+            $image = $request->file('fav_icon');
+            $imageName = 'icon'.'.'.'png';
+            $path = 'frontend/images/';
+            $image->move($path,$imageName);
+           
+        }
+
         if($request->hasFile('image')){
             $image = $request->file('image');
             $imageName = uniqid().$image->getClientOriginalName();
@@ -24,12 +33,12 @@ class TextController extends Controller
             $image->move($path,$imageName);
             $url = $path.$imageName;
 
-
+            $text->image = $url;
 
         }
 
         $text->name = $request->name;
-        $text->image = $url;
+        
         $text->save();
         Session::flash('info','Info save');
         return back();
